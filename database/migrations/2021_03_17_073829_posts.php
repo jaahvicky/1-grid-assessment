474d463edeabef522507dec82df53b00bc642c0a
Victor Musvibe <jaahvicky@gmail.com>
2021-03-17 11:24:23 +0200

    add Model

---
+++ database/migrations/2021_03_17_073829_posts.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Posts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    // posts table
+    Schema::create('posts', function (Blueprint $table) {
+        $table->id();
+        $table->string('title')->unique();
+        $table->text('body');
+        $table->string('slug')->unique();
+        $table->timestamps();
+      });
+  
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
